84bf079bf361008d68468dfb9460cba6eb113b8f
nikhilc <nikhilc.developer@gmail.com>
2016-03-01 15:14:42 +0000

    added comment controller spec and modified custom.css.scss file

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe CommentsController, type: :controller do
-
+  describe "POST #create" do
+    before do
+      @nik = User.create!(email: "nik@gmail.com", password: "password")
+    end
+    
+    context "signed in user" do
+      it "can create a comment" do
+        login_user @nik
+        article = Article.create( title: "First Title", body: "Awesome Article", user: @nik )
+        article_id = article.id
+        expect(flash[:success]).to eq("Comment has been created")
+      end
+    end
+    
+    context "Non signed in user" do
+      it "redirected to sign in page" do
+        login_user nil
+        @article = Article.create(title: "first article", body: "Awesome article", user: @nik)
+        post :create, comment: { body: "Awesome post" }
+        article_id = article.id
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
+
